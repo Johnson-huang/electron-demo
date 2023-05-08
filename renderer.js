ce1c65329ab42647a279faad50af9a540718d27f
Johnson-huang <2678683520@qq.com>
2023-05-08 22:42:23 +0800

    nodemon

--- renderer.js
+++ renderer.js
@@ -3,7 +3,7 @@ information.innerText = `本应用正在使用 Chrome (v${versions.chrome()}), N
 
 const func = async () => {
     const response = await window.versions.ping()
-    // alert(response)
+    alert(response)
     // console.log(response) // 打印 'pong'
 }
 
