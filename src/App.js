adf2cc81b740555c3508d8f2d46362aac6b10fde
Johnson-huang <2678683520@qq.com>
2023-05-13 15:16:22 +0800

    code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,120 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState } from 'react'
+
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// import "easymde/dist/easymde.min.css"
+
+// import { faPlus, faFileImport } from '@fortawesome/free-solid-svg-icons'
+// import SimpleMDE from "react-simplemde-editor"
+// import uuidv4 from 'uuid/v4'
+import { flattenArr, objToArr, timestampToString } from './utils/helper'
+import fileHelper from './utils/fileHelper'
 
 import FileSearch from "./components/FileSearch";
+import FileList from "./components/FileList";
+// import BottomBtn from './components/BottomBtn'
+// import TabList from './components/TabList'
+// import Loader from './components/Loader'
+
+import useIpcRenderer from './hooks/useIpcRenderer'
+
+const { join, basename, extname, dirname } = window.require('path')
+const { remote, ipcRenderer } = window.require('electron')
+const Store = window.require('electron-store')
+const fileStore = new Store({'name': 'Files Data'})
+const settingsStore = new Store({name: 'Settings'})
+const getAutoSync = () => ['accessKey', 'secretKey', 'bucketName', 'enableAutoSync'].every(key => !!settingsStore.get(key))
+const saveFilesToStore = (files) => {
+  // we don't have to store any info in file system, eg: isNew, body ,etc
+  const filesStoreObj = objToArr(files).reduce((result, file) => {
+    const { id, path, title, createdAt, isSynced, updatedAt } = file
+    result[id] = {
+      id,
+      path,
+      title,
+      createdAt,
+      isSynced,
+      updatedAt
+    }
+    return result
+  }, {})
+  fileStore.set('files', filesStoreObj)
+}
 
 function App() {
+  const [ files, setFiles ] = useState(fileStore.get('files') || {})
+  const [ activeFileID, setActiveFileID ] = useState('')
+  const [ openedFileIDs, setOpenedFileIDs ] = useState([])
+  const [ unsavedFileIDs, setUnsavedFileIDs ] = useState([])
+  const [ searchedFiles, setSearchedFiles ] = useState([])
+  const [ isLoading, setLoading ] = useState(false)
+  const filesArr = objToArr(files)
+  const savedLocation = settingsStore.get('savedFileLocation') || remote.app.getPath('documents')
+  const activeFile = files[activeFileID]
+  const openedFiles = openedFileIDs.map(openID => {
+    return files[openID]
+  })
+  const fileListArr = (searchedFiles.length > 0) ? searchedFiles : filesArr
+
   const fileSearch = (keyword) => {
-    // filter out the new files based on the keyword
     // const newFiles = filesArr.filter(file => file.title.includes(keyword))
     // setSearchedFiles(newFiles)
+  }
+
+  const fileClick = (fileID) => {
+    // set current active file
+    setActiveFileID(fileID)
+    const currentFile = files[fileID]
+    const { id, title, path, isLoaded } = currentFile
+    if (!isLoaded) {
+      if (getAutoSync()) {
+        ipcRenderer.send('download-file', { key: `${title}.md`, path, id })
+      } else {
+        fileHelper.readFile(currentFile.path).then(value => {
+          const newFile = { ...files[fileID], body: value, isLoaded: true }
+          setFiles({ ...files, [fileID]: newFile })
+        })
+      }
+    }
+    // if openedFiles don't have the current ID
+    // then add new fileID to openedFiles
+    if (!openedFileIDs.includes(fileID)) {
+      setOpenedFileIDs([ ...openedFileIDs, fileID ])
+    }
+  }
+
+  const deleteFile = (id) => {
+    if (files[id].isNew) {
+      const { [id]: value, ...afterDelete } = files
+      setFiles(afterDelete)
+    } else {
+      fileHelper.deleteFile(files[id].path).then(() => {
+        const { [id]: value, ...afterDelete } = files
+        setFiles(afterDelete)
+        saveFilesToStore(afterDelete)
+        // close the tab if opened
+        tabClose(id)
+      })
+    }
+  }
+  const updateFileName = (id, title, isNew) => {
+    const newPath = isNew ? join(savedLocation, `${title}.md`)
+        : join(dirname(files[id].path), `${title}.md`)
+    const modifiedFile = { ...files[id], title, isNew: false, path: newPath }
+    const newFiles = { ...files, [id]: modifiedFile }
+    if (isNew) {
+      fileHelper.writeFile(newPath, files[id].body).then(() => {
+        setFiles(newFiles)
+        saveFilesToStore(newFiles)
+      })
+    } else {
+      const oldPath = files[id].path
+      fileHelper.renameFile(oldPath, newPath).then(() => {
+        setFiles(newFiles)
+        saveFilesToStore(newFiles)
+      })
+    }
+
   }
 
   return (
@@ -17,6 +124,12 @@ function App() {
           <FileSearch
             title='My Document'
             onFileSearch={fileSearch}
+          />
+          <FileList
+              files={fileListArr}
+              onFileClick={fileClick}
+              onFileDelete={deleteFile}
+              onSaveEdit={updateFileName}
           />
         </div>
         <div className="col-9 bg-primary right-panel">
